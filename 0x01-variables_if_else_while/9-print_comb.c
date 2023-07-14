049e810ee14a2a171d5f9122acc4f820243e0203
Maso <nkombisa.masonwabe@gmail.com>
2023-07-14 19:39:17 +0200

    01/add: scrip-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - prints out the numbers to 10
+  *
+  * Return: 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+		}
+		putchar(32);
+	}
+	putchar('\n');
+
+	return (0);
+}
