a52ebb0148ee222cb8ff200a71f496ef7d419955
Maso <nkombisa.masonwabe@gmail.com>
2023-07-14 07:56:23 +0200

    hw00/add: script-101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: prints the specified string to the screen.
+  * Return: zero
+  */
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (0);
+}
