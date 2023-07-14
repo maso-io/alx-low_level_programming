568b819991b51d424b9524e3f3215b6c12e8e2e1
Maso <nkombisa.masonwabe@gmail.com>
2023-07-14 19:14:30 +0200

    01/add: scrip-6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - prints out the numbers to 10
+  *
+  * Return: 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
