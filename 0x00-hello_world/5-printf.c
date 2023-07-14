d01112c511cbd2cd8b835f4b6acb6d8ec3ecf3a2
Maso <nkombisa.masonwabe@gmail.com>
2023-07-14 06:00:34 +0200

    hl00/add: scripts5&6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: prints the specified string to the screen.
+  * Return: zero
+  */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,with proper
+grammar, but the outcome is a piece of art,\n");
+return (0);
+}
