0a39f55467984a6df58667ad8bf9fb921c5ff13a
Maso <nkombisa.masonwabe@gmail.com>
2023-07-15 01:25:02 +0200

    01/add: script-101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main - prints out the numbers to 10
+  *
+  * Return: 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				if (i == j == k || i > j > k)
+				{
+					continue;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 56 && j == 57 && k == 57)
+				{
+					putchar('\n');
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+
+			}
+		}
+	}
+
+	return (0);
+}
