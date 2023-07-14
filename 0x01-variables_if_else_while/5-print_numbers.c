60090d5a9772ef775258fed28a66ab7ac1fb64cd
Maso <nkombisa.masonwabe@gmail.com>
2023-07-14 19:08:57 +0200

    01/add: scrip-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - prints out the numbers to 10
+  *
+  * Return: 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
