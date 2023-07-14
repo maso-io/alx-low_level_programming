420440867030d88cd00514d9f2808b47a06c78ba
Maso <nkombisa.masonwabe@gmail.com>
2023-07-14 13:33:21 +0200

    v01/add: script-0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Check's if value of n > | < | = 0
+ *
+ * Return: 0 (end of main) 
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+    if (n < 0)
+    {
+            printf("%d is negetive\n", n);
+    }
+    else if (n > 0)
+    {
+            printf("%d is positive\n", n);
+    }
+    else{
+            printf("%d is zero\n", n);
+    }
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+### Part II: Intro to logic
