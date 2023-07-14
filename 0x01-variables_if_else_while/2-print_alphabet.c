3f4a8cdb12c1968945f96a4a6ed505dd962c7fc0
Maso <nkombisa.masonwabe@gmail.com>
2023-07-14 18:45:46 +0200

    01/mod: scrip2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+  * main - prints out the letters of the alphabet
+  *
+  * Return: 0 (Success)
+  *
+  */
+
 int main(void)
 {
 	int i;
@@ -7,7 +14,6 @@ int main(void)
 	for (i = 97; i <= 122; i++)
 	{
 		putchar(i);
-		putchar('\n');
 	}
 	return (0);
 }
