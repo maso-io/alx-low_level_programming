a4ac000b7df398ce78de53db955d4665eda6f26c
Maso <nkombisa.masonwabe@gmail.com>
2023-07-18 17:51:25 +0200

    02/mod: script-7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,4 @@
+#include<limits.h>
 #include <stdlib.h>
 #include "main.h"
 
@@ -10,7 +11,8 @@
 
 int print_last_digit(int n)
 {
-	int tmp = abs(n) % 10;
+	int tmp_raw = abs(n);
+	int tmp = tmp_raw % 10;
 
 	_putchar(tmp);
 	return (tmp);
