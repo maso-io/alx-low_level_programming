2b2b2f12d084b18e82389bc0cc6c2f4c17f86f26
Maso <nkombisa.masonwabe@gmail.com>
2023-07-18 14:03:56 +0200

    02/mod: script-1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,7 +12,7 @@ void print_alphabet(void)
 	char alphabets[] = {
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		, 'z', '\n'
+		 'z', '\n'
 	};
 
 	for (i = 0; i < 25; i++)
