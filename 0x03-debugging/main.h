cdcbc4490ff1724f1e7a5155632c88c8cd7ec30e
Maso <nkombisa.masonwabe@gmail.com>
2023-07-21 03:37:23 +0200

    03/mod: main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,9 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/* check if its +/- */
 void positive_or_negative(int i);
+
+/* check MAX */
 int largest_number(int a, int b, int c);
+
+/* prints remaining */
 void print_remaining_days(int month, int day, int year);
 
 #endif
