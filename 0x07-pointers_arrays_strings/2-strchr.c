b814d5b61275d558f7f136fedc2d2f6c569673bc
Maso <nkombisa.masonwabe@gmail.com>
2023-08-01 04:45:39 +0200

    07/mod: scripts

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,9 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p;
 
-	p = s;
-	for (i = 0; s[i] != c; i++)
-		p++;
-	return (p++);
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (&s[i]);
+	return ('\0');
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,21 +11,16 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
-	char *p;
 
-	p = 0x00;
 	for (i = 0; accept[i] != '\0'; i++)
 	{
-		if (p != 0x00)
-			break;
 		for (j = 0; s[j] != '\0'; j++)
 		{
 			if (s[j] == accept[i])
 			{
-				p = &s[j];
-				break;
+				return (&s[j]);
 			}
 		}
 	}
-	return (p);
+	return (0x00);
 }
