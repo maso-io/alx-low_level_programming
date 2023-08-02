1d833ce297bb203e354d716141b46de09fba8abe
Maso <nkombisa.masonwabe@gmail.com>
2023-08-02 21:16:16 +0200

    08/add: Readme|header|script

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,5 @@
+## 0x08. C - Recursion
+
+This directory contains tasks which will emphasise the following on Recursion:
+1. What is recursion
+2. How to implement recursion
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/* prints char to stdout */
+int _putchar(char);
+/* prints a string followed by new line */
+void _puts_recursion(char *s);
+/* prints a string in reverse */
+void _print_rev_recursion(char *s);
+/* returns length of string */
+int _strlen_recursion(char *s);
+/* returns the factorial of a given number */
+int factorial(int n);
+/* return x raised to power y */
+int _pow_recursion(int x, int y);
+/* returns the square-root of a number */
+int _sqrt_recursion(int n);
+/* checks if integer is a prime number */
+int is_prime_number(int n);
+
+#endif
