447d40e7031e37e9f765d994fa94a5d980f0f74e
Maso <nkombisa.masonwabe@gmail.com>
2023-08-05 03:16:41 +0200

    0A/mod: script 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,10 +15,7 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 
 	if (argc == 1)
-	{
 		printf("%d\n", 0);
-		return (1);
-	}
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
@@ -26,8 +23,12 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		if (atoi(argv[i]) < 0)
+			sum += (atoi(argv[i]) * -1);
+		else
+			sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	if (sum > 0)
+		printf("%d\n", sum);
 	return (0);
 }
