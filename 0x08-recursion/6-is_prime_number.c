85cff2d1d9a2d2ecdd01f7a2bbeeb2672a8c9d62
masonwabe <nkombisa.masonwabe@gmail.com>
2023-08-07 06:28:31 -0700

    08/add: script 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+int check_prime(int n, int index);
+/**
+ * is_prime - check if number is a prime
+ * @n: number to check
+ *
+ * Return: 1 is number is prime, otherwise 0.
+ */
+
+int is_prime(int n)
+{
+}
+
+/**
+ * check_prime - check if number is prime
+ * @n: number to check
+ * @index: index to check number against
+ *
+ * Return: 1 if prime, otherwise 0.
+ */
+
+int check_prime(int n, int index)
+{
+}
