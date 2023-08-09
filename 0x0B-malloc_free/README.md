47d42315f1ef6ceca5f96fea89ec97d36ed0dae9
Maso <nkombisa.masonwabe@gmail.com>
2023-08-10 00:39:11 +0200

    0B/add: script|Readme

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+### Demonstration on Dynamic memory allocation
+How to use malloc and or free.
+Or any other dynamic allocation functions
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+/* function that creates an array of chars, and init it with given char */
+char *create_array(unsigned int, char);
+
+#endif
