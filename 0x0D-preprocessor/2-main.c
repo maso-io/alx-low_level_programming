ac0d6fbb34760d9da3ff3068cc80fde6441c002c
Maso <nkombisa.masonwabe@gmail.com>
2023-08-15 00:36:34 +0200

    0D/add: script|Readme

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints name of current file
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
