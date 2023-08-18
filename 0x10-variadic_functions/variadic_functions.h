da1ac89259c2d99137153bb85a4eac093f02a3a5
Maso <nkombisa.masonwabe@gmail.com>
2023-08-18 02:25:12 +0200

    10/add: script | Readme

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _VARFUNC_
+#define _VARFUNC_
+
+#include <stdarg.h>
+#include <stddef.h>
+
+/* writes the character c to stdout */
+int _putchar(char c);
+/* return sum of all parameters */
+int sum_them_all(const unsigned int n, ...);
+
+#endif
