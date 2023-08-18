3e68b45b3e072da38a1ec419e9ce42309869eab9
Maso <nkombisa.masonwabe@gmail.com>
2023-08-18 04:45:50 +0200

    10/mod: script

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,32 +9,37 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	size_t i, j, len;
-	const char specifier[] = "cifs";
+	size_t i, len;
+	char *s;
 
 	i = 0;
 	len = strlen(format);
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
-		j = 0;
-		while (j < 4)
+		switch (format[i])
 		{
-			if (specifier[j] == format[i])
-			{
-				if (specifier[j] == 'c')
-					printf("%c", va_arg(ap, int));
-				else if (specifier[j] == 'i')
-					printf("%d", va_arg(ap, int));
-				else if (specifier[j] == 'f')
-					printf("%f", va_arg(ap, double));
-				else if (specifier[j] == 's')
-					printf("%s", va_arg(ap, char *));
-				if (j != 3 && i != len)
-					printf(", ");
-			}
-			j++;
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				((s != NULL) ? printf("%s", s) : printf("(nil)"));
+				break;
 		}
+		if (i != len - 1 && (
+				format[i] == 'c' ||
+				 format[i] == 'i' ||
+				 format[i] == 'f' ||
+				 format[i] == 's')
+			)
+			printf(", ");
 		i++;
 	}
 	printf("\n");
