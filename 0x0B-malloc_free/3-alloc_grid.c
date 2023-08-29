9400ddef7c5d53934fca40ec0d934c7aac5195ac
Maso <nkombisa.masonwabe@gmail.com>
2023-08-29 19:12:46 +0200

    0B/mod: script

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,18 +9,26 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j, **mem;
+	int i, j, k, **mem;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	mem = malloc(height * sizeof(int *));
+	mem = (int **)malloc(height * sizeof(int *));
 	if (mem == NULL)
+	{
+		free(mem);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
-		*(mem + i) = malloc(width * sizeof(int));
+		*(mem + i) = (int *)malloc(width * sizeof(int));
 		if (*(mem + i) == NULL)
+		{
+			for (k = i; k >= 0; --i)
+				free(*(mem - k));
+			free(mem);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			mem[i][j] = 0;
 	}
