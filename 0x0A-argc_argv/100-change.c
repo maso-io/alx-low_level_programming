86a8b4ad5588d4a7496ad52c58e2de32705265b1
Maso <nkombisa.masonwabe@gmail.com>
2023-09-30 13:24:35 +0200

    0A/mod: change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 	{
 		if (atoi(argv[1]) < 0)
 			printf("0\n");
-		if (atoi(argv[1]))
+		if (atoi(argv[1]) && atoi(argv[1]) > 0)
 		{
 			cents = atoi(argv[1]);
 			count = change(cents);
