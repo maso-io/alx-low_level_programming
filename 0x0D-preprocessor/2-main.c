a756a2b26eaa32d4fa1b3a6b643b4d0d339ae6ec
Maso <nkombisa.masonwabe@gmail.com>
2023-10-10 00:14:13 +0200

    0D/add: 2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of program it was compile by
+ *
+ * Return: Always 0 on sucess
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
