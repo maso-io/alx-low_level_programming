b92a5adf20bf7144c3c5450aee08dee179d2bca3
Maso <nkombisa.masonwabe@gmail.com>
2023-10-23 08:58:29 +0200

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void __attribute__((constructor)) start_up();
+
+/**
+ * start_up - constructor func that executes before main
+ */
+void start_up(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
