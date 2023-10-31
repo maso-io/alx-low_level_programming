9211f6b3f9fe0aee4c0d69d6ed533b4e3e930543
Maso <nkombisa.masonwabe@gmail.com>
2023-10-31 03:48:03 +0200

    mod file 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * read_textfile - reads textfile and print it to stdout
  * @filename: name of the file
@@ -8,21 +9,50 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	size_t size;
+	int fd, count, rd_letters;
 	char *buffer;
-
+	if (!filename)
+		return (0);
 	buffer = (char *)malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if (!filename | !buffer | (fd == -1))
+	if (fd == EOF)
+	{
+		free(buffer);
 		return (0);
-	size = read(fd, buffer, letters);
-	if ((int)size < 0)
-		return (0);
-	size = write(STDOUT_FILENO, buffer, size);
+	}
+	rd_letters = read(fd, buffer, letters);
+	printf("\n\tread size: %lu\tletters: %lu\tstrlen(buffer): %ld\n", size, letters, strlen(buffer));
 	close(fd);
-	if (size != letters)
+	if (re_letters == EOF)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	for (count = 0; count < rd_letters; count++)
+	{
+		if (write(fd, &buffer[count], 1) == EOF)
+		{
+			close(fp);
+			free(buffer);
+			return (0);
+		}
+	}
+	close(sp);
+	free(buffer);
+	/*
+	while (--size > 0 && write(STDOUT_FILENO, buffer, 1) != 0 && *buffer++ !=
+			EOF)
+	{
+		count++;
+	}
+	size = write(STDOUT_FILENO, buffer, size);
+	write(STDOUT_FILENO, buffer, 1);*/
+	printf("\n\tread size: %lu\tletters: %lu\tstrlen(buffer): %ld\n", size, letters, strlen(buffer));
+	if (rd_letters != letters)
 		return (0);
 
-	return ((ssize_t)size);
+	return (rd_letters);
 }
