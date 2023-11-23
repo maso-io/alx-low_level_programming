c2b539f68af04967f380528cb68e2b4a52685410
masonwabe nkombisa <nkombisa.masonwabe@gmail.com>
2023-11-23 15:55:29 +0000

    file- 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to convert
+ */
+void print_binary(ul_int n)
+{
+	ul_int mask, shift, num;
+
+	num = n;
+	mask = 1;
+	shift = 0;
+	if (num == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (num > 1)
+	{
+		shift++;
+		num >>= 1;
+	}
+	mask <<= shift;
+	num = n;
+	while (mask)
+	{
+		if ((num & mask) & mask)
+			_putchar('1');
+		else if (((num & mask) & mask) == 0)
+			_putchar('0');
+		else
+			return;
+		mask >>= 1;
+	}
+}
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _putchar - writes character to std out
+ * @c: character to write
+ *
+ * Return: number of character writen
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
