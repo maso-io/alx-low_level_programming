8d9ef25594c653b341e4e6ec5180682281e1f5b1
Maso <nkombisa.masonwabe@gmail.com>
2023-12-02 11:32:47 +0200

    mod \n

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,7 +30,7 @@ int main(int ac, char *argv[])
 	else
 		ret = *fds;
 	if (ret == 98)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	if (ret == 99)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 
