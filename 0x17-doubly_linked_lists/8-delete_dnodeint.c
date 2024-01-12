cf6faf539fc9d949625ec324ccf560062c83266c
Maso <nkombisa.masonwabe@gmail.com>
2024-01-12 23:15:43 +0200

    add file 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,5 +8,21 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *head;
+	u_int node;
+	dlistint_t *h;
+
+	if (!head)
+		return (-1);
+	node = index;
+	h = *head;
+	while (index-- && h)
+		h = h->next;
+	if (!h)
+		return (-1);
+	if (node != 0)
+		h->prev->next = h->next;
+	else
+		*head = h->next;
+
+	return (1);
 }
