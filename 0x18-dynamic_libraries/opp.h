8e757bd46082ca4f59327a9e1376d19d492787ec
Maso <nkombisa.masonwabe@gmail.com>
2024-01-29 15:21:21 +0200

    add c-lib for python

---
+++ 0x18-dynamic_libraries/opp.h
@@ -0,0 +1,11 @@
+#ifndef _OPPPYTHON
+#define _OPPPYTHON
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif /* END OPERATION FROM PYTHON */
